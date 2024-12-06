f9ca48eb874dbd2b7f7387f2414409f7e24863fb
Teerapat (Bosck) <56102118+bxsck@users.noreply.github.com>
2024-12-07 00:30:42 +0700

    chore: change mongo config

--- api/src/config/database.config.ts
+++ api/src/config/database.config.ts
@@ -2,8 +2,9 @@ import { DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mongodb',
-  url: 'mongodb+srv://bxsck:ELLUbaXrjMNWiQFb@swipe-test.23j2j.mongodb.net/?retryWrites=true&w=majority&appName=swipe-test',
+  url: 'mongodb+srv://bxsck:ELLUbaXrjMNWiQFb@swipe-test.23j2j.mongodb.net/?ssl=true&retryWrites=true&w=majority&appName=swipe-test',
   // database: process.env.DATABASE_NAME,
+  ssl: true,
   database: 'task-management',
   synchronize: true,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
